41f78be7323c7847352a3cebd08aa1dcff2055c4
Dmitry Baev <baev@users.noreply.github.com>
2025-01-14 10:58:59 +0000

    add more logs to failing test

--- packages/core/test/store/convert.test.ts
+++ packages/core/test/store/convert.test.ts
@@ -1,5 +1,5 @@
 import { md5 } from "@allurereport/plugin-api";
-import { attachment } from "allure-js-commons";
+import { attachment, step } from "allure-js-commons";
 import { describe, expect, it } from "vitest";
 import type { StateData } from "../../src/store/convert.js";
 import { testResultRawToState } from "../../src/store/convert.js";
@@ -8,23 +8,40 @@ const emptyStateData: StateData = { testCases: new Map(), attachments: new Map()
 
 const readerId = "convert.test.ts";
 
+const wrap = <T extends (...args: any) => any, P extends Parameters<T>, R extends ReturnType<T>>(
+  f: T,
+): ((...args: P) => Promise<R>) => {
+  return async (...args: P): Promise<R> => {
+    return await step(f.name, async (): Promise<R> => {
+      for (let i = 0; i < args.length; i++) {
+        await attachment(`arg${i}`, JSON.stringify(args[i], null, 2), "application/json");
+      }
+      const result: R = f(...args);
+      await attachment("result", JSON.stringify(result, null, 2), "application/json");
+      return result;
+    });
+  };
+};
+
+const functionUnderTest = wrap(testResultRawToState).bind(this);
+
 describe("testResultRawToState", () => {
   it("should set default name", async () => {
-    const result = testResultRawToState(emptyStateData, {}, { readerId });
+    const result = await functionUnderTest(emptyStateData, {}, { readerId });
     expect(result).toMatchObject({
       name: "Unknown test",
     });
   });
 
   it("should set default status", async () => {
-    const result = testResultRawToState(emptyStateData, {}, { readerId });
+    const result = await functionUnderTest(emptyStateData, {}, { readerId });
     expect(result).toMatchObject({
       status: "unknown",
     });
   });
 
   it("should set undefined history id for tests without testId or fullName", async () => {
-    const result = testResultRawToState(emptyStateData, {}, { readerId });
+    const result = await functionUnderTest(emptyStateData, {}, { readerId });
     expect(result).toMatchObject({
       historyId: undefined,
     });
@@ -32,7 +49,7 @@ describe("testResultRawToState", () => {
 
   it("should calculate historyId based on testId", async () => {
     const testId = "a test id";
-    const result = testResultRawToState(emptyStateData, { testId }, { readerId });
+    const result = await functionUnderTest(emptyStateData, { testId }, { readerId });
     expect(result).toMatchObject({
       historyId: `${md5(testId)}.${md5("")}`,
     });
@@ -40,7 +57,7 @@ describe("testResultRawToState", () => {
 
   it("should calculate historyId based on fullName", async () => {
     const fullName = "a test full name";
-    const result = testResultRawToState(emptyStateData, { fullName }, { readerId });
+    const result = await functionUnderTest(emptyStateData, { fullName }, { readerId });
     expect(result).toMatchObject({
       historyId: `${md5(fullName)}.${md5("")}`,
     });
@@ -49,7 +66,7 @@ describe("testResultRawToState", () => {
   it("should calculate historyId based on testId if both testId and fullName is present", async () => {
     const testId = "a test id";
     const fullName = "a test full name";
-    const result = testResultRawToState(emptyStateData, { fullName, testId }, { readerId });
+    const result = await functionUnderTest(emptyStateData, { fullName, testId }, { readerId });
     expect(result).toMatchObject({
       historyId: `${md5(testId)}.${md5("")}`,
     });
@@ -63,7 +80,7 @@ describe("testResultRawToState", () => {
         value: "second",
       },
     ];
-    const result = testResultRawToState(emptyStateData, { testId, parameters }, { readerId });
+    const result = await functionUnderTest(emptyStateData, { testId, parameters }, { readerId });
     expect(result).toMatchObject({
       historyId: `${md5(testId)}.${md5("first:second")}`,
     });
@@ -85,7 +102,7 @@ describe("testResultRawToState", () => {
         value: "3",
       },
     ];
-    const result = testResultRawToState(emptyStateData, { testId, parameters }, { readerId });
+    const result = await functionUnderTest(emptyStateData, { testId, parameters }, { readerId });
     expect(result).toMatchObject({
       historyId: `${md5(testId)}.${md5("a:2,b:3,c:1")}`,
     });
@@ -108,7 +125,7 @@ describe("testResultRawToState", () => {
         excluded: true,
       },
     ];
-    const result = testResultRawToState(emptyStateData, { testId, parameters }, { readerId });
+    const result = await functionUnderTest(emptyStateData, { testId, parameters }, { readerId });
     expect(result).toMatchObject({
       historyId: `${md5(testId)}.${md5("a:2,c:1")}`,
     });
@@ -116,14 +133,14 @@ describe("testResultRawToState", () => {
 
   it("should omit empty parameters array from history id calculation", async () => {
     const testId = "a test id";
-    const result = testResultRawToState(emptyStateData, { testId, parameters: [] }, { readerId });
+    const result = await functionUnderTest(emptyStateData, { testId, parameters: [] }, { readerId });
     expect(result).toMatchObject({
       historyId: `${md5(testId)}.${md5("")}`,
     });
   });
 
-  it("should detect attachment link content type based on file extension if specified", () => {
-    const result = testResultRawToState(
+  it("should detect attachment link content type based on file extension if specified", async () => {
+    const result = await functionUnderTest(
       emptyStateData,
       { steps: [{ type: "attachment", originalFileName: "some-file.txt" }] },
       { readerId },
@@ -140,13 +157,11 @@ describe("testResultRawToState", () => {
   });
 
   it("should set extension based on file name", async () => {
-    const result = testResultRawToState(
+    const result = await functionUnderTest(
       emptyStateData,
       { steps: [{ type: "attachment", originalFileName: "some-file.txt" }] },
       { readerId },
     );
-
-    await attachment("result", JSON.stringify(result, null, 2), "application/json");
 
     const [attach] = result.steps;
     expect(attach).toMatchObject({
