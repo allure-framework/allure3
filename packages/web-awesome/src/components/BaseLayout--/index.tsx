4532365e86790b67ba2eceb6c3232e2069aea58a
Todti <hello@iamtodt.com>
2025-02-06 10:28:40 +0100

    awesome wip split view

--- packages/web-awesome/src/components/BaseLayout--/index.tsx
+++ packages/web-awesome/src/components/BaseLayout--/index.tsx
@@ -4,7 +4,6 @@ import { useEffect } from "preact/compat";
 import { Footer } from "@/components/Footer";
 import MainReport from "@/components/MainReport";
 import Modal from "@/components/Modal";
-import SideBySide from "@/components/SideBySide";
 import TestResult from "@/components/TestResult";
 import { fetchStats, getLocale, getTheme } from "@/stores";
 import { fetchPieChartData } from "@/stores/chart";
@@ -36,7 +35,7 @@ export const BaseLayout = ({ testResultId }: BaseLayoutProps) => {
     }
   }, [testResultId]);
 
-  const testResult = testResultId ? (
+  const content = testResultId ? (
     <Loadable
       source={testResultStore}
       renderLoader={() => <PageLoader />}
@@ -52,40 +51,11 @@ export const BaseLayout = ({ testResultId }: BaseLayoutProps) => {
       )}
     />
   ) : (
-    <div>notest</div>
-  );
-
-  const Main = () => (
     <div className={styles.wrapper}>
       <Loadable source={treeStore} renderLoader={() => <PageLoader />} renderData={() => <MainReport />} />
       <Footer />
     </div>
   );
 
-  // const content = testResultId ? (
-  //   <Loadable
-  //     source={testResultStore}
-  //     renderLoader={() => <PageLoader />}
-  //     transformData={(data) => data[testResultId]}
-  //     renderData={(testResult) => (
-  //       <>
-  //         <Modal testResult={testResult} />
-  //         <div className={styles.wrapper} key={testResult?.id}>
-  //           <TestResult testResult={testResult} />
-  //           <Footer />
-  //         </div>
-  //       </>
-  //     )}
-  //   />
-  // ) : (
-  //   <div className={styles.wrapper}>
-  //     <Loadable source={treeStore} renderLoader={() => <PageLoader />} renderData={() => <MainReport />} />
-  //     <Footer />
-  //   </div>
-  // );
-  return <SideBySide left={<Main />} right={testResult} />;
-
-  // {
-  /* return <div className={styles.layout}>{content}</div>;*/
-  // }
+  return <div className={styles.layout}>{content}</div>;
 };
