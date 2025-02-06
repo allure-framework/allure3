71a01fdfdeec789d3841fa8a9f4cb1aea46a6a87
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-02-07 00:02:43 +0700

    xcresult: split models/cli between reader, new API, legacy API

--- packages/reader/src/xcresult/xcresulttool/cli.ts
+++ packages/reader/src/xcresult/xcresulttool/cli.ts
@@ -1,28 +1,25 @@
-import { invokeCliTool, invokeJsonCliTool } from "../toolRunner.js";
-import type { XcTestActivityCollection, XcTestDetails, XcTestResultCollection } from "./model.js";
+import console from "node:console";
+import { invokeCliTool, invokeJsonCliTool } from "../../toolRunner.js";
+import type { XcActivities, XcTestDetails, XcTests } from "./model.js";
 
 export const xcrun = async <T>(utilityName: string, ...args: readonly string[]) => {
-  return await invokeJsonCliTool<T>("xcrun", [utilityName, ...args], { timeout: 1000 });
+  try {
+    return await invokeJsonCliTool<T>("xcrun", [utilityName, ...args], { timeout: 1000 });
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export const xcresulttool = async <T>(...args: readonly string[]) => await xcrun<T>("xcresulttool", ...args);
 
 export const getTests = async (xcResultPath: string) =>
-  await xcresulttool<XcTestResultCollection>("get", "test-results", "tests", "--path", xcResultPath);
+  await xcresulttool<XcTests>("get", "test-results", "tests", "--path", xcResultPath);
 
 export const getTestDetails = async (xcResultPath: string, testId: string) =>
   await xcresulttool<XcTestDetails>("get", "test-results", "test-details", "--test-id", testId, "--path", xcResultPath);
 
 export const getTestActivities = async (xcResultPath: string, testId: string) =>
-  await xcresulttool<XcTestActivityCollection>(
-    "get",
-    "test-results",
-    "activities",
-    "--test-id",
-    testId,
-    "--path",
-    xcResultPath,
-  );
+  await xcresulttool<XcActivities>("get", "test-results", "activities", "--test-id", testId, "--path", xcResultPath);
 
 export const exportAttachments = async (xcResultPath: string, outputPath: string) => {
   await invokeCliTool("xcrun", [
