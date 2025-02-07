d00d838b6c7da5db44f9b938b8587b2c21f49c8e
viperwow <dmitrii.spirin@qameta.io>
2025-02-07 16:25:30 +0400

    feat(web-components): add-chart-component
    - Changed `TrendChart` to be in a separated storybook block `Charts`

--- packages/web-components/stories/TrendChart.stories.tsx
+++ packages/web-components/stories/TrendChart.stories.tsx
@@ -4,7 +4,7 @@ import type { TrendChartDataItem } from "@allurereport/web-components";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof TrendChart> = {
-  title: "Commons/TrendChart",
+  title: "Charts/TrendChart",
   component: TrendChart,
 };
 
