02d629aad53533e4bf0a40d13c1c23a0cb0f43c7
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-02-25 01:26:26 +0700

    fix: attachments export and lookup

--- packages/reader/src/xcresult/xcresulttool/utils.ts
+++ packages/reader/src/xcresult/xcresulttool/utils.ts
@@ -1,6 +1,7 @@
 import { BufferResultFile } from "@allurereport/reader-api";
 import console from "node:console";
 import { mkdtemp, readFile, rm } from "node:fs/promises";
+import { tmpdir } from "node:os";
 import path from "node:path";
 import { exportAttachments } from "./cli.js";
 import type { AttachmentFileFactory } from "./model.js";
@@ -11,7 +12,7 @@ export const parseWithExportedAttachments = async (
 ) => {
   let attachmentsDir: string | undefined;
   try {
-    attachmentsDir = await mkdtemp("allure-");
+    attachmentsDir = await mkdtemp(path.join(tmpdir(), "allure-reader-xcresult-"));
     await exportAttachments(xcResultPath, attachmentsDir);
     await fn(createAttachmentFileFactoryFn(attachmentsDir));
   } finally {
@@ -28,11 +29,37 @@ export const parseWithExportedAttachments = async (
 const createAttachmentFileFactoryFn =
   (attachmentsDir: string): AttachmentFileFactory =>
   async (attachmentUuid, uniqueFileName) => {
+    const fileExtension = path.extname(uniqueFileName);
     const attachmentFilePath = path.join(attachmentsDir, attachmentUuid);
-    try {
-      const content = await readFile(attachmentFilePath);
-      return new BufferResultFile(content, uniqueFileName);
-    } catch (e) {
-      console.error("Can't read attachment", attachmentUuid, "in", attachmentsDir, ":", e);
+
+    const [firstAttemptOk, firstAttemptResult] = await tryReadFile(attachmentFilePath, uniqueFileName);
+    if (firstAttemptOk) {
+      return firstAttemptResult;
     }
+
+    const errors: unknown[] = [firstAttemptResult];
+
+    if ((firstAttemptResult as any)?.code === "ENOENT" && fileExtension) {
+      const attachmentPathWithExt = `${attachmentFilePath}${fileExtension}`;
+      const [secondAttemptOk, secondAttemptResult] = await tryReadFile(attachmentPathWithExt, uniqueFileName);
+
+      if (secondAttemptOk) {
+        return secondAttemptResult;
+      }
+
+      errors.push(secondAttemptResult);
+    }
+
+    console.error("Can't read attachment", attachmentUuid, "in", attachmentsDir, ":", ...errors);
   };
+
+const tryReadFile = async (
+  attachmentPath: string,
+  attachmentName: string,
+): Promise<[true, BufferResultFile] | [false, unknown]> => {
+  try {
+    return [true, new BufferResultFile(await readFile(attachmentPath), attachmentName)];
+  } catch (e) {
+    return [false, e];
+  }
+};
