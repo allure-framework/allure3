da633f30585d55e593a12821ca51f57f84ba1013
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-02-25 01:22:54 +0700

    feat: check xcresulttool availibility

--- packages/reader/src/xcresult/index.ts
+++ packages/reader/src/xcresult/index.ts
@@ -1,6 +1,7 @@
 import type { ResultsVisitor } from "@allurereport/reader-api";
 import { DirectoryResultsReader } from "@allurereport/reader-api";
 import console from "node:console";
+import { version } from "./xcresulttool/cli.js";
 import newApi from "./xcresulttool/index.js";
 import { legacyApiUnavailable } from "./xcresulttool/legacy/cli.js";
 import legacyApi from "./xcresulttool/legacy/index.js";
@@ -13,30 +14,58 @@ class XcResultReader extends DirectoryResultsReader {
   }
 
   override async readDirectory(visitor: ResultsVisitor, resultDir: string): Promise<boolean> {
-    if (resultDir.endsWith(".xfresult")) {
-      try {
-        await parseWithExportedAttachments(resultDir, async (createAttachmentFile) => {
-          const context = { xcResultPath: resultDir, createAttachmentFile };
-
-          try {
-            this.#tryApi(visitor, legacyApi, context);
-            return;
-          } catch (e) {
-            if (!legacyApiUnavailable()) {
-              throw e;
-            }
-          }
-
-          this.#tryApi(visitor, newApi, context);
-        });
-
-        return true;
-      } catch (e) {
-        console.error("error parsing", resultDir, e);
+    if (resultDir.endsWith(".xcresult")) {
+      if (await this.#xcResultToolAvailable()) {
+        return await this.#parseBundleWithXcResultTool(visitor, resultDir);
       }
     }
     return false;
   }
+
+  #xcResultToolAvailable = async () => {
+    try {
+      await version();
+      return true;
+    } catch (e) {
+      console.error(
+        "xcresulttool is unavailable on this machine. Please, make sure XCode is installed. The original error:",
+        e,
+      );
+    }
+
+    return false;
+  };
+
+  #parseBundleWithXcResultTool = async (visitor: ResultsVisitor, xcResultPath: string) => {
+    try {
+      await parseWithExportedAttachments(xcResultPath, async (createAttachmentFile) => {
+        const context = { xcResultPath: xcResultPath, createAttachmentFile };
+
+        try {
+          await this.#tryApi(visitor, legacyApi, context);
+          return;
+        } catch (e) {
+          console.error(e);
+          if (!legacyApiUnavailable()) {
+            // The legacy API available but some other error has occured. We should not attempt using the new API in
+            // that case because the results may've been partially created.
+            throw e;
+          }
+        }
+
+        // The legacy API is not available. Fallback to the new API (as paradoxical as it may sound; the new API is
+        // much less convenient to consume, lacks some important information, and hides test results that share the
+        // same test id.
+        await this.#tryApi(visitor, newApi, context);
+      });
+
+      return true;
+    } catch (e) {
+      console.error("error parsing", xcResultPath, e);
+    }
+
+    return false;
+  };
 
   #tryApi = async (visitor: ResultsVisitor, generator: ApiParseFunction, context: ParsingContext) => {
     const { xcResultPath: originalFileName } = context;
--- packages/reader/src/xcresult/xcresulttool/cli.ts
+++ packages/reader/src/xcresult/xcresulttool/cli.ts
@@ -1,5 +1,5 @@
 import console from "node:console";
-import { invokeCliTool, invokeJsonCliTool } from "../../toolRunner.js";
+import { invokeCliTool, invokeJsonCliTool, invokeStdoutCliTool } from "../../toolRunner.js";
 import type { XcActivities, XcTestDetails, XcTests } from "./xcModel.js";
 
 export const xcrun = async <T>(utilityName: string, ...args: readonly string[]) => {
@@ -11,6 +11,15 @@ export const xcrun = async <T>(utilityName: string, ...args: readonly string[]) 
 };
 
 export const xcresulttool = async <T>(...args: readonly string[]) => await xcrun<T>("xcresulttool", ...args);
+
+export const version = async () => {
+  const stdout = invokeStdoutCliTool("xcrun", ["xcresulttool", "--version"], { timeout: 1000 });
+  const lines: string[] = [];
+  for await (const line of stdout) {
+    lines.push(line);
+  }
+  return lines.join("\n");
+};
 
 export const getTests = async (xcResultPath: string) =>
   await xcresulttool<XcTests>("get", "test-results", "tests", "--path", xcResultPath);
