0fe642e39d991607e5b36716c0f61f0fffd5c09c
viperwow <dmitrii.spirin@qameta.io>
2025-02-25 16:41:27 +0400

    feat(TrendChart): Add support for different chart interaction modes
    - Introduce `TrendChartKind `enum to define chart interaction modes
    - Create utility function to generate chart configuration based on interaction kind to keep code clean
    - Update `TrendChart `component to support different chart modes
    - Add Storybook story demonstrating slice interaction mode
    - Refactor types to improve component flexibility and drop mutually exclusive properties

--- packages/web-components/src/components/TrendChart/TrendChart.tsx
+++ packages/web-components/src/components/TrendChart/TrendChart.tsx
@@ -1,19 +1,24 @@
 import { ResponsiveLine } from "@nivo/line";
 import type { FunctionalComponent } from "preact";
 import { defaultTrendChartConfig } from "./config";
-import type { TrendChartProps } from "./types";
+import { type TrendChartProps, TrendChartKind } from "./types";
+import { getKindConfig } from "./utils";
 
 export const TrendChart: FunctionalComponent<TrendChartProps> = ({
+  kind = TrendChartKind.mesh,
   width = 600,
   height = 400,
   rootArialLabel,
   ...restProps
 }) => {
+  const kindConfig = getKindConfig(kind);
+
   return (
     // Accessible container for the trend diagram
     <div role="img" aria-label={rootArialLabel} tabIndex={0} style={{ width, height }}>
       <ResponsiveLine
         {...defaultTrendChartConfig}
+        {...kindConfig}
         {...restProps}
       />
     </div>
