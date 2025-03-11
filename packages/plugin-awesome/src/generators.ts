6c100f46665787e867bb8cad7d4a92cccf6ef24d
viperwow <dmitrii.spirin@qameta.io>
2025-03-12 01:37:47 +0400

    refactor(generators): Update type for Allure awesome data writer
    - Change type annotation from `AllureAwesomeDataWriter` to `AwesomeDataWriter`

--- packages/plugin-awesome/src/generators.ts
+++ packages/plugin-awesome/src/generators.ts
@@ -376,7 +376,7 @@ export const generateStaticFiles = async (
 };
 
 export const generateHistoryTrendData = async (
-  writer: AllureAwesomeDataWriter,
+  writer: AwesomeDataWriter,
   reportName: string,
   statistic: Statistic,
   testResults: TestResult[],
