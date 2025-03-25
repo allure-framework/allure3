cdf06d56e4571f0d9b1770e97293eed10400e90f
Todti <hello@iamtodt.com>
2025-03-25 13:18:17 +0100

    awesome: categories ui

---
+++ packages/web-awesome/src/components/Categories/index.tsx
@@ -0,0 +1,84 @@
+import { Button, Loadable, PageLoader, Text, Tree } from "@allurereport/web-components";
+import { useEffect } from "preact/hooks";
+import type { AwesomeStatus } from "types";
+import { statsStore } from "@/stores";
+import {
+  categoriesStore,
+  clearCategoriesFilters,
+  fetchCategoriesData,
+  filteredCategories,
+  noTests,
+  noTestsFound,
+  setCategoriesStatus,
+} from "@/stores/categories";
+import { useI18n } from "@/stores/locale";
+import { navigateTo } from "@/stores/router";
+import { currentTab } from "@/stores/tabs";
+import { collapsedTrees, toggleTree, treeFiltersStore } from "@/stores/tree";
+import * as styles from "./styles.scss";
+
+const Categories = () => {
+  const { t } = useI18n("empty");
+
+  useEffect(() => {
+    fetchCategoriesData();
+  }, []);
+
+  useEffect(() => {
+    setCategoriesStatus(currentTab.value as AwesomeStatus);
+  }, [currentTab.value]);
+
+  return (
+    <Loadable
+      source={categoriesStore}
+      renderLoader={() => <PageLoader />}
+      renderData={() => {
+        if (noTests.value) {
+          return (
+            <div className={styles["tree-list"]}>
+              <div className={styles["tree-empty-results"]}>
+                <Text className={styles["tree-empty-results-title"]}>{t("no-results")}</Text>
+              </div>
+            </div>
+          );
+        }
+
+        if (noTestsFound.value) {
+          return (
+            <div className={styles["tree-list"]}>
+              <div className={styles["tree-empty-results"]}>
+                <Text tag="p" className={styles["tree-empty-results-title"]}>
+                  {t("no-tests-found")}
+                </Text>
+                <Button
+                  className={styles["tree-empty-results-clear-button"]}
+                  type="button"
+                  text={t("clear-filters")}
+                  size={"s"}
+                  style={"outline"}
+                  onClick={() => clearCategoriesFilters()}
+                />
+              </div>
+            </div>
+          );
+        }
+
+        return (
+          <div className={styles["tree-list"]}>
+            <Tree
+              collapsedTrees={collapsedTrees.value}
+              toggleTree={toggleTree}
+              treeFiltersStore={treeFiltersStore}
+              navigateTo={navigateTo}
+              statsStore={statsStore}
+              tree={filteredCategories.value}
+              statusFilter={currentTab.value as AwesomeStatus}
+              root
+            />
+          </div>
+        );
+      }}
+    />
+  );
+};
+export default Categories;
--- packages/web-awesome/src/components/ReportBody/HeaderActions.tsx
+++ packages/web-awesome/src/components/ReportBody/HeaderActions.tsx
@@ -1,4 +1,4 @@
-import { SearchBox } from "@allurereport/web-components";
+import { Button, DropdownButton, Menu, SearchBox, allureIcons } from "@allurereport/web-components";
 import { useI18n } from "@/stores/locale";
 import { setTreeQuery, treeQuery } from "@/stores/treeFilters";
 import { Filters } from "./Filters";
@@ -11,9 +11,39 @@ const Search = () => {
   return <SearchBox placeholder={t("search-placeholder")} value={query} onChange={setTreeQuery} />;
 };
 
+const TreeToggler = () => {
+  const { t } = useI18n("ui");
+
+  const handleSelect = (selectedOption: any) => {
+    // setLocale(selectedOption);
+  };
+  const treeOptions = [
+    { key: "Suites", value: "ekke" },
+    { key: "Categories", value: "ekke" },
+  ];
+
+  return (
+    <Menu
+      size="s"
+      menuTrigger={({ isOpened, onClick }) => (
+        <DropdownButton style="outline" size="m" text={"Categories"} isExpanded={isOpened} onClick={onClick} />
+      )}
+    >
+      <Menu.Section>
+        {treeOptions.map(({ key, value }) => (
+          <Menu.ItemWithCheckmark onClick={() => handleSelect(key)} key={key} isChecked={true}>
+            {value}
+          </Menu.ItemWithCheckmark>
+        ))}
+      </Menu.Section>
+    </Menu>
+  );
+};
+
 export const HeaderActions = () => {
   return (
     <div className={styles.headerActions}>
+      <TreeToggler />
       <Search />
       <Filters />
     </div>
