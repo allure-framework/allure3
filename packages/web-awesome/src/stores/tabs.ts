431f9e23df736e00df80655fc65505818e7fb00f
Todti <hello@iamtodt.com>
2025-03-25 18:58:02 +0100

    awesome: categories ui

--- packages/web-awesome/src/stores/tabs.ts
+++ packages/web-awesome/src/stores/tabs.ts
@@ -1,7 +1,17 @@
 import { signal } from "@preact/signals";
+import { setTreeStatus } from "@/stores/tree";
+import { treeFiltersStore } from "@/stores/treeFilters";
+import type { AwesomeStatus } from "../../types";
 
-export const currentTab = signal<string | undefined>("total");
+export const currentTab = signal<string | undefined>(treeFiltersStore.value.status || "total");
 
 export const setCurrentTab = (tab: string) => {
+  if (tab === currentTab.value) {
+    currentTab.value = "total";
+    setTreeStatus(currentTab.value as AwesomeStatus);
+    return;
+  }
+
   currentTab.value = tab;
+  setTreeStatus(currentTab.value as AwesomeStatus);
 };
