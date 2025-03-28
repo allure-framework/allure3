f50f2a74c3df016c3cabda57effde27af16f4467
epszaw <konstantin@epishev.me>
2025-03-28 15:17:41 +0100

    complete summary ui

--- packages/web-summary/src/components/MetadataRow/index.tsx
+++ packages/web-summary/src/components/MetadataRow/index.tsx
@@ -9,7 +9,7 @@ export type MetadataRowProps = {
 export const MetadataRow: FunctionalComponent<MetadataRowProps> = ({ label, children }) => {
   return (
     <div className={styles["metadata-row"]}>
-      <Text type={"ui"} size={"s"} bold>
+      <Text type={"ui"} size={"s"} bold className={styles.label}>
         {label}
       </Text>
       <Text type={"ui"} size={"s"}>
