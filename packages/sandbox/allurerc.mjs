39fb9a508b07916ef4b5f87532419733e3f90011
viperwow <dmitrii.spirin@qameta.io>
2025-04-09 02:48:45 +0400

    feat(sandbox): Add dashboards configuration to enhance reporting capabilities
    - Introduced dashboards configuration in sandbox's allurerc.mjs to support additional trend and pie chart visualizations.
    - Updated reportName and layout options for improved customization and clarity in dashboard reporting.

--- packages/sandbox/allurerc.mjs
+++ packages/sandbox/allurerc.mjs
@@ -5,7 +5,7 @@ export default defineConfig({
   output: "./allure-report",
   historyPath: "./history.jsonl",
   plugins: {
-    "allure2": {
+    allure2: {
       options: {
         reportName: "HelloWorld",
         singleFile: false,
@@ -25,6 +25,53 @@ export default defineConfig({
         singleFile: false,
         reportLanguage: "en",
         open: false,
+      },
+    },
+    dashboards: {
+      options: {
+        singleFile: false,
+        reportName: "HelloWorld-Dashboards",
+        reportLanguage: "en",
+        layout: [
+          {
+            type: "trend",
+            dataType: "status",
+            mode: "percent",
+          },
+          {
+            type: "trend",
+            dataType: "status",
+            limit: 10,
+          },
+          {
+            title: "Custom Status Trend",
+            type: "trend",
+            dataType: "status",
+            mode: "percent",
+            limit: 15,
+          },
+          {
+            type: "trend",
+            dataType: "status",
+            limit: 15,
+            metadata: {
+              executionIdFn: (executionOrder) => `build-${executionOrder}`,
+              executionNameFn: (executionOrder) => `build #${executionOrder}`,
+            },
+          },
+          {
+            type: "trend",
+            dataType: "severity",
+            limit: 15,
+          },
+          {
+            type: "pie",
+          },
+          {
+            type: "pie",
+            title: "Custom Pie",
+          },
+        ],
       },
     },
     csv: {
