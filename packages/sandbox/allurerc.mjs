28bb82189afb22609b9cfeddb342b325b2e5f265
viperwow <dmitrii.spirin@qameta.io>
2025-04-09 03:45:38 +0400

    feat(sandbox): Enhance dashboard layout with additional trend and pie chart configurations
    - Added new trend and pie chart configurations to the dashboard layout in allurerc.mjs, improving data visualization options.
    - Introduced custom titles and metadata for trend charts to enhance clarity and usability.
    - Expanded the layout to include multiple trend and pie chart types, allowing for more comprehensive reporting capabilities.

--- packages/sandbox/allurerc.mjs
+++ packages/sandbox/allurerc.mjs
@@ -36,7 +36,40 @@ export default defineConfig({
           {
             type: "trend",
             dataType: "status",
+            mode: "percent",
+          },
+          {
+            type: "trend",
+            dataType: "status",
             limit: 10,
+          },
+          {
+            title: "Custom Status Trend",
+            type: "trend",
+            dataType: "status",
+            mode: "percent",
+            limit: 15,
+          },
+          {
+            type: "trend",
+            dataType: "status",
+            limit: 15,
+            metadata: {
+              executionIdFn: (executionOrder) => `build-${executionOrder}`,
+              executionNameFn: (executionOrder) => `build #${executionOrder}`,
+            },
+          },
+          {
+            type: "trend",
+            dataType: "severity",
+            limit: 15,
+          },
+          {
+            type: "pie",
+          },
+          {
+            type: "pie",
+            title: "Custom Pie",
           },
         ],
       },
