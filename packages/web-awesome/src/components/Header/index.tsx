1fdc51118dd3b0e490b4aa1469b757ba41a9d3b1
viperwow <dmitrii.spirin@qameta.io>
2025-04-10 23:54:29 +0400

    refactor(web-awesome): Replace EnvironmentPicker, LanguagePicker, ToggleLayout, and ThemeButton with HeaderControls in Header and TrHeader components
    - Updated Header and TrHeader components to use HeaderControls for improved structure and maintainability.
    - Removed individual imports for EnvironmentPicker, LanguagePicker, ToggleLayout, and ThemeButton.

--- packages/web-awesome/src/components/Header/index.tsx
+++ packages/web-awesome/src/components/Header/index.tsx
@@ -1,13 +1,10 @@
 import type { ClassValue } from "clsx";
 import clsx from "clsx";
-import { EnvironmentPicker } from "@/components/EnvironmentPicker";
-import { LanguagePicker } from "@/components/LanguagePicker";
 import { TrBreadcrumbs } from "@/components/TestResult/TrHeader/TrBreadcrumbs";
-import { ThemeButton } from "@/components/ThemeButton/ThemeButton";
-import ToggleLayout from "@/components/ToggleLayout";
 import { route } from "@/stores/router";
 import { testResultStore } from "@/stores/testResults";
 import * as styles from "./styles.scss";
+import { HeaderControls } from "@/components/HeaderControls";
 
 interface HeaderProps {
   className?: ClassValue;
@@ -19,12 +16,7 @@ export const Header = ({ className }: HeaderProps) => {
   return (
     <div className={clsx(styles.above, className)}>
       {id && <TrBreadcrumbs testResult={testResultStore.value?.data?.[id]} />}
-      <div className={styles.right}>
-        <EnvironmentPicker />
-        <LanguagePicker />
-        <ToggleLayout />
-        <ThemeButton />
-      </div>
+      <HeaderControls className={styles.right} />
     </div>
   );
 };
--- packages/web-awesome/src/components/TestResult/TrHeader/index.tsx
+++ packages/web-awesome/src/components/TestResult/TrHeader/index.tsx
@@ -1,23 +1,15 @@
 import type { FunctionalComponent } from "preact";
-import { EnvironmentPicker } from "@/components/EnvironmentPicker";
-import { LanguagePicker } from "@/components/LanguagePicker";
 import type { TrProps } from "@/components/TestResult";
 import { TrBreadcrumbs } from "@/components/TestResult/TrHeader/TrBreadcrumbs";
-import { ThemeButton } from "@/components/ThemeButton/ThemeButton";
-import ToggleLayout from "@/components/ToggleLayout";
 import { isSplitMode } from "@/stores/layout";
 import * as styles from "./styles.scss";
+import { HeaderControls } from "@/components/HeaderControls";
 
 export const TrHeader: FunctionalComponent<TrProps> = ({ testResult }) => {
   return (
     <div className={styles.above}>
       {!isSplitMode.value ? <TrBreadcrumbs testResult={testResult} /> : ""}
-      <div className={styles.right}>
-        <EnvironmentPicker />
-        <LanguagePicker />
-        <ToggleLayout />
-        <ThemeButton />
-      </div>
+      <HeaderControls className={styles.right} />
     </div>
   );
 };
