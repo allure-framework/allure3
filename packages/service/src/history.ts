ceb6965b7e421039b704e120857e3f0678cd4ee5
epszaw <konstantin@epishev.me>
2025-04-30 11:54:36 +0200

    change history reading flow

--- packages/service/src/history.ts
+++ packages/service/src/history.ts
@@ -1,25 +1,29 @@
-import type { AllureHistory, HistoryDataPoint } from "@allurereport/core-api";
-import type { AllureStore } from "@allurereport/plugin-api";
-import type { AllureService } from "./service.js";
+import type { AllureHistory, HistoryDataPoint, RepoData } from "@allurereport/core-api";
+import type { Config } from "@allurereport/plugin-api";
+import { AllureService } from "./service.js";
 
 export class AllureRemoteHistory implements AllureHistory {
+  readonly #service: AllureService;
+
   constructor(
-    private readonly service: AllureService,
-    private readonly store: AllureStore,
-  ) {}
+    allureServiceConfig: Config["allureService"],
+    private readonly repoDataAccessor: () => Promise<RepoData | undefined>,
+  ) {
+    this.#service = new AllureService(allureServiceConfig);
+  }
 
   async readHistory() {
-    const repoData = await this.store.repoData();
+    const repoData = await this.repoDataAccessor();
 
-    return await this.service.downloadHistory({
+    return await this.#service.downloadHistory({
       branch: repoData?.branch,
     });
   }
 
   async appendHistory(data: HistoryDataPoint) {
-    const repoData = await this.store.repoData();
+    const repoData = await this.repoDataAccessor();
 
-    await this.service.appendHistory({
+    await this.#service.appendHistory({
       history: data,
       branch: repoData?.branch,
     });
