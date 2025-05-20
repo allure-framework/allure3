b6fbd193141863bfa2fc59c2662027b0960f2f92
epszaw <konstantin@epishev.me>
2025-05-20 14:08:54 +0200

    fix problems with summary page generation

--- packages/e2e/test/commons/summary.test.ts
+++ packages/e2e/test/commons/summary.test.ts
@@ -1,15 +1,97 @@
 import AwesomePlugin from "@allurereport/plugin-awesome";
 import { expect, test } from "@playwright/test";
 import { Stage, Status } from "allure-js-commons";
+import { CommonPage, SummaryPage } from "../pageObjects/index.js";
 import { AwesomePluginWithoutSummary, type ReportBootstrap, bootstrapReport } from "../utils/index.js";
 
 let bootstrap: ReportBootstrap;
+let summaryPage: SummaryPage;
+let commonPage: CommonPage;
 
 test.afterAll(async () => {
   await bootstrap?.shutdown?.();
 });
 
+test.beforeEach(async ({ page }) => {
+  commonPage = new CommonPage(page);
+  summaryPage = new SummaryPage(page);
+});
+
 test.describe("summary", () => {
+  test("should not generate summary for a single report", async ({ page }) => {
+    bootstrap = await bootstrapReport({
+      reportConfig: {
+        name: "Sample allure report",
+        appendHistory: false,
+        history: undefined,
+        historyPath: undefined,
+        knownIssuesPath: undefined,
+        plugins: [
+          {
+            id: "awesome1",
+            enabled: true,
+            plugin: new AwesomePlugin({}),
+            options: {},
+          },
+        ],
+      },
+      testResults: [
+        {
+          name: "0 sample passed test",
+          fullName: "sample.js#0 sample passed test",
+          status: Status.PASSED,
+          stage: Stage.FINISHED,
+          start: 1000,
+        },
+      ],
+    });
+
+    await page.goto(bootstrap.url);
+
+    await expect(summaryPage.reportCardLocator).not.toBeVisible();
+    await expect(commonPage.baseLayoutLocator).toBeVisible();
+  });
+
+  test("should not generate summary on the single report re-generation", async ({ page }) => {
+    bootstrap = await bootstrapReport({
+      reportConfig: {
+        name: "Sample allure report",
+        appendHistory: false,
+        history: undefined,
+        historyPath: undefined,
+        knownIssuesPath: undefined,
+        plugins: [
+          {
+            id: "awesome1",
+            enabled: true,
+            plugin: new AwesomePlugin({}),
+            options: {},
+          },
+        ],
+      },
+      testResults: [
+        {
+          name: "0 sample passed test",
+          fullName: "sample.js#0 sample passed test",
+          status: Status.PASSED,
+          stage: Stage.FINISHED,
+          start: 1000,
+        },
+      ],
+    });
+
+    await page.goto(bootstrap.url);
+
+    await expect(summaryPage.reportCardLocator).not.toBeVisible();
+    await expect(commonPage.baseLayoutLocator).toBeVisible();
+
+    await bootstrap.regenerate();
+    await page.reload();
+
+    await expect(summaryPage.reportCardLocator).not.toBeVisible();
+    await expect(commonPage.baseLayoutLocator).toBeVisible();
+  });
+
   test("should render cards for each generated report with summary", async ({ page, context }) => {
     bootstrap = await bootstrapReport({
       reportConfig: {
@@ -43,18 +125,17 @@ test.describe("summary", () => {
         },
       ],
     });
-    const summaryCardsLocator = page.getByTestId("summary-report-card");
 
     await page.goto(bootstrap.url);
 
-    await expect(summaryCardsLocator).toHaveCount(2);
+    await expect(summaryPage.reportCardLocator).toHaveCount(2);
 
     const pageUrl = page.url();
 
     expect(context.pages()).toHaveLength(1);
     expect(context.pages()[0].url()).toBe(pageUrl);
 
-    const [newTab] = await Promise.all([context.waitForEvent("page"), await summaryCardsLocator.nth(0).click()]);
+    const [newTab] = await Promise.all([context.waitForEvent("page"), await summaryPage.clickReportCard(0)]);
 
     expect(context.pages()[0].url()).toBe(pageUrl);
     expect(newTab.url()).not.toBe(`${pageUrl}awesome1/index.html`);
@@ -99,10 +180,9 @@ test.describe("summary", () => {
         },
       ],
     });
-    const summaryCardsLocator = page.getByTestId("summary-report-card");
 
     await page.goto(bootstrap.url);
 
-    await expect(summaryCardsLocator).toHaveCount(2);
+    await expect(summaryPage.reportCardLocator).toHaveCount(2);
   });
 });
--- packages/e2e/test/pageObjects/index.ts
+++ packages/e2e/test/pageObjects/index.ts
@@ -1,3 +1,4 @@
 export * from "./Tree.js";
 export * from "./TestResult.js";
 export * from "./Common.js";
+export * from "./Summary.js";
