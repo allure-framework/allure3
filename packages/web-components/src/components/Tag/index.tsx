d2088edb9fcb0b3b74f120374ec447fab2407146
viperwow <dmitrii.spirin@qameta.io>
2025-05-22 13:33:29 +0400

    feat/web-components: enhance Tag component with new 'kind' prop and corresponding styles

--- packages/web-components/src/components/Tag/index.tsx
+++ packages/web-components/src/components/Tag/index.tsx
@@ -3,12 +3,15 @@ import styles from "./styles.scss";
 import clsx from "clsx";
 import { Text } from "@/components/Typography";
 
+export type TagKind = "successful" | "failed" | "warning" | "neutral";
+
 export interface TagProps {
   className?: string;
+  kind?: TagKind;
 }
 
-export const Tag: FunctionalComponent<TagProps> = ({ className, children }) => (
-  <Text className={clsx(styles.tag, className)} bold size="s" type="ui">
+export const Tag: FunctionalComponent<TagProps> = ({ className, kind, children }) => (
+  <Text className={clsx(styles.tag, className, kind && styles[kind])} bold size="s" type="ui">
     {children}
   </Text>
 );
