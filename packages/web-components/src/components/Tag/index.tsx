2187d2f4ef972b500653f89f5af859a569fa584a
viperwow <dmitrii.spirin@qameta.io>
2025-05-22 13:52:28 +0400

    refactor/web-components: rename 'kind' prop to 'skin' in Tag component for improved clarity

--- packages/web-components/src/components/Tag/index.tsx
+++ packages/web-components/src/components/Tag/index.tsx
@@ -6,6 +6,7 @@ import styles from "./styles.scss";
 export type TagSkin = "successful" | "failed" | "warning" | "neutral";
 
 export interface TagProps {
+<<<<<<< HEAD
   "className"?: string;
   "skin"?: TagSkin;
   "data-testid"?: string;
@@ -13,6 +14,14 @@ export interface TagProps {
 
 export const Tag: FunctionComponent<TagProps> = ({ className, skin, children, "data-testid": testId }) => (
   <Text className={clsx(styles.tag, className, skin && styles[skin])} bold size="s" type="ui" data-testid={testId}>
+=======
+  className?: string;
+  skin?: TagSkin;
+}
+
+export const Tag: FunctionalComponent<TagProps> = ({ className, skin, children }) => (
+  <Text className={clsx(styles.tag, className, skin && styles[skin])} bold size="s" type="ui">
+>>>>>>> 430d655ca (refactor/web-components: rename 'kind' prop to 'skin' in Tag component for improved clarity)
     {children}
   </Text>
 );
