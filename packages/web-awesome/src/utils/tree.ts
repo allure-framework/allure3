da29983f7cf365cb63e783975f2fdbb67df233a2
viperwow <dmitrii.spirin@qameta.io>
2025-05-26 18:03:01 +0400

    refactor/web-awesome: enhance filter functionality by adding new transition states and updating related components for improved status representation

---
+++ packages/web-awesome/src/utils/tree.ts
@@ -0,0 +1,13 @@
+import type { RecursiveTree } from "@allurereport/web-components/global";
+import type { AwesomeTreeLeaf } from "types";
+
+export const createLeafLocalizer = (t: (data: string) => string) => (leaf: AwesomeTreeLeaf): AwesomeTreeLeaf => ({
+    ...leaf,
+    transitionText: t(leaf.transition),
+});
+
+export const createTreeLocalizer = (t: (data: string) => string) => (tree: RecursiveTree): RecursiveTree => ({
+    ...tree,
+    leaves: tree.leaves.length ? tree.leaves.map(createLeafLocalizer(t)) : tree.leaves,
+    trees: tree.trees.length ? tree.trees.map(createTreeLocalizer(t)) : tree.trees,
+});
