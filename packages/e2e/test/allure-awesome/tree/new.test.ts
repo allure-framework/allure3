58361dbad77f21a3d0e92dadf5ce51cc3ddfc3f1
viperwow <dmitrii.spirin@qameta.io>
2025-05-28 14:21:20 +0400

    refactor/e2e: enhance TreePage to support new test status and update related tests for improved metadata handling

--- packages/e2e/test/allure-awesome/tree/new.test.ts
+++ packages/e2e/test/allure-awesome/tree/new.test.ts
@@ -2,8 +2,10 @@ import { expect, test } from "@playwright/test";
 import { Stage, Status, label } from "allure-js-commons";
 import { makeHistoryId, makeTestCaseId } from "../../utils/index.js";
 import { type ReportBootstrap, bootstrapReport } from "../utils/index.js";
+import { TreePage } from "../../pageObjects/index.js";
 
 let bootstrap: ReportBootstrap;
+let treePage: TreePage;
 
 const reportName = "Sample allure report";
 const flakyTestName = "Test with history";
@@ -105,6 +107,9 @@ test.describe("new tests", () => {
 
   test.beforeEach(async ({ browserName, page }) => {
     await label("env", browserName);
+
+    treePage = new TreePage(page);
+
     await page.goto(bootstrap.url);
   });
 
@@ -112,54 +117,41 @@ test.describe("new tests", () => {
     await bootstrap?.shutdown?.();
   });
 
-  test("should be able to filter new tests with using new filter", async ({ page }) => {
-    await expect(page.getByTestId("tree-leaf")).toHaveCount(3);
+  test("should be able to filter new tests with using new filter", async () => {
+    await expect(treePage.leafLocator).toHaveCount(3);
 
-    // Open filters
-    await page.getByTestId("filters-button").click();
+    // Select filter by new status
+    await treePage.toggleNewFilter();
 
-    // Select retry filter
-    await page.getByTestId("new-filter").click();
+    // Verify only tests with new status are visible
+    await expect(treePage.leafLocator).toHaveCount(2);
 
-    // Verify only tests with retries are visible
-    const treeLeaves = page.getByTestId("tree-leaf");
-    await expect(treeLeaves).toHaveCount(2);
+    // Verify the test names are correct for tests with new status
+    await expect(treePage.getLeafByTitle(passedTestName)).toBeVisible();
+    await expect(treePage.getLeafByTitle(failedTestName)).toBeVisible();
+    await expect(treePage.getLeafByTitle(flakyTestName)).not.toBeVisible();
 
-    // Verify the test names are correct for tests with retries
-    await expect(page.getByText(passedTestName, { exact: true })).toBeVisible();
-    await expect(page.getByText(failedTestName, { exact: true })).toBeVisible();
-    await expect(page.getByText(flakyTestName, { exact: true })).not.toBeVisible();
-
-    // Disable retry filter
-    await page.getByTestId("new-filter").click();
+    // Disable filter by new status
+    await treePage.toggleNewFilter();
 
     // Verify all tests are visible again
-    await expect(page.getByTestId("tree-leaf")).toHaveCount(3);
+    await expect(treePage.leafLocator).toHaveCount(3);
   });
 
-  test("should show new icon only for new tests in the tree", async ({ page }) => {
-    const treeLeaves = page.getByTestId("tree-leaf");
-
+  test("should show new icon only for new tests in the tree", async () => {
     // Classic new test
-    const passedNewTest = treeLeaves
-      .filter({ has: page.getByText(passedTestName, { exact: true }) })
-      .getByTestId("tree-leaf-transition-new");
-    await expect(passedNewTest).toBeVisible();
-
-    const failedNewTest = treeLeaves
-      .filter({ has: page.getByText(failedTestName, { exact: true }) })
-      .getByTestId("tree-leaf-transition-new");
-    await expect(failedNewTest).toBeVisible();
+    await expect(treePage.getLeafByTitle(passedTestName).getByTestId("tree-leaf-transition-new")).toBeVisible();
+    await expect(treePage.getLeafByTitle(failedTestName).getByTestId("tree-leaf-transition-new")).toBeVisible();
 
     // Non-new test
-    const nonNew = treeLeaves
-      .filter({ has: page.getByText(flakyTestName, { exact: true }) })
-      .getByTestId("tree-leaf-transition-new");
-    await expect(nonNew).not.toBeVisible();
+    await expect(treePage.getLeafByTitle(flakyTestName).getByTestId("tree-leaf-transition-new")).not.toBeVisible();
   });
 
-  test("metadata shows correct count of new tests", async ({ page }) => {
-    await expect(page.getByTestId("metadata-item-total").getByTestId("metadata-value")).toHaveText("3");
-    await expect(page.getByTestId("metadata-item-new").getByTestId("metadata-value")).toHaveText("2");
+  test("metadata shows correct count of new tests", async () => {
+    const total = await treePage.getMetadataValue("total");
+    const newCount = await treePage.getMetadataValue("new");
+
+    expect(total).toBe("3");
+    expect(newCount).toBe("2");
   });
 });
