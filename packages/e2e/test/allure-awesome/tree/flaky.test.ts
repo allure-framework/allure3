f81e695790e58aa5e579c277bfe8753947141fb7
viperwow <dmitrii.spirin@qameta.io>
2025-06-17 16:19:37 +0400

    refactor(e2e): simplify flaky test setup by consolidating mock utility functions and improving test result generation

--- packages/e2e/test/allure-awesome/tree/flaky.test.ts
+++ packages/e2e/test/allure-awesome/tree/flaky.test.ts
@@ -1,139 +1,94 @@
 import { expect, test } from "@playwright/test";
 import { Stage, Status, label } from "allure-js-commons";
 import { TreePage } from "../../pageObjects/index.js";
-import { makeHistoryId, makeTestCaseId } from "../utils/mocks.js";
 import { type ReportBootstrap, bootstrapReport } from "../utils/index.js";
+import {
+  makeTestCaseId,
+  makeHistoryId,
+  makeTestResult,
+  makeHistory,
+  makeHistoryTestResults,
+  makeReportConfig,
+  makeTestResults,
+} from "../utils/mocks.js";
 
 let bootstrap: ReportBootstrap;
 let treePage: TreePage;
 
+const reportName = "Sample allure report with flaky tests";
+const flakyTestName = "Classic flaky test";
+const flakyTestFullname = "sample.js#Classic flaky test";
+const flakyTestCaseId = makeTestCaseId(flakyTestFullname);
+const flakyHistoryId = makeHistoryId(flakyTestFullname);
+
+const nonFlakyTestName = "Non-flaky test";
+const nonFlakyTestFullname = "sample.js#Non-flaky test";
+const nonFlakyTestCaseId = makeTestCaseId(nonFlakyTestFullname);
+const nonFlakyHistoryId = makeHistoryId(nonFlakyTestFullname);
+
 test.describe("flaky", () => {
   test.beforeAll(async () => {
-    const now = Date.now();
-
-    const reportName = "Sample allure report with flaky tests";
-    const flakyTestFullname = "sample.js#Classic flaky test";
-    const flakyTestName = "Classic flaky test";
-    const testCaseId = makeTestCaseId(flakyTestFullname);
-    const historyId = makeHistoryId(flakyTestFullname);
-
-    bootstrap = await bootstrapReport({
-      reportConfig: {
-        name: reportName,
-        appendHistory: true,
-        history: [
-          {
-            uuid: "dc4d9432-ebef-4e0f-b121-37fcf0383023",
-            name: reportName,
-            timestamp: 1745926867897,
-            knownTestCaseIds: [testCaseId, "54511e33b8d1887a829f815f468563a9"],
-            testResults: {
-              [historyId]: {
-                id: "42dffbf3bb12f89807c85206c3f993a2",
-                name: flakyTestName,
-                fullName: flakyTestFullname,
-                status: Status.FAILED,
-                start: 1745926873782,
-                stop: 1745926874782,
-                duration: 1000,
-                labels: [],
-              },
-              "54511e33b8d1887a829f815f468563a9.d41d8cd98f00b204e9800998ecf8427e": {
-                id: "181c008c6d519b30bd3279dded351bd3",
-                name: "Non-flaky test",
-                fullName: "sample.js#Non-flaky test",
-                status: "passed",
-                start: 1745926877782,
-                stop: 1745926878782,
-                duration: 1000,
-                labels: [],
-              },
-            },
-            metrics: {},
-          },
-          {
-            uuid: "b441fbc8-5222-4380-a325-d776436789f3",
-            name: reportName,
-            timestamp: 1745926884436,
-            knownTestCaseIds: [testCaseId, "54511e33b8d1887a829f815f468563a9"],
-            testResults: {
-              "54511e33b8d1887a829f815f468563a9.d41d8cd98f00b204e9800998ecf8427e": {
-                id: "67175da7a4eded923ad6d8dda76f2838",
-                name: "Non-flaky test",
-                fullName: "sample.js#Non-flaky test",
-                status: "passed",
-                start: 1745926894322,
-                stop: 1745926895322,
-                duration: 1000,
-                labels: [],
-              },
-              [historyId]: {
-                id: "1373936b78555e2d7646b6f7eccb5b83",
-                name: flakyTestName,
-                fullName: flakyTestFullname,
-                status: Status.PASSED,
-                start: 1745926890322,
-                stop: 1745926891322,
-                duration: 1000,
-                labels: [],
-              },
-            },
-            metrics: {},
-          },
-          {
-            uuid: "47b06933-e2a4-401a-b873-648885f033a2",
-            name: reportName,
-            timestamp: 1745926887428,
-            knownTestCaseIds: [testCaseId, "54511e33b8d1887a829f815f468563a9"],
-            testResults: {
-              "54511e33b8d1887a829f815f468563a9.d41d8cd98f00b204e9800998ecf8427e": {
-                id: "b40702a85e54a2f8dcc6cfdf791170dd",
-                name: "Non-flaky test",
-                fullName: "sample.js#Non-flaky test",
-                status: "passed",
-                start: 1745926897309,
-                stop: 1745926898309,
-                duration: 1000,
-                labels: [],
-              },
-              [historyId]: {
-                id: "218637ff0c630613e70a149cda54bdf2",
-                name: flakyTestName,
-                fullName: flakyTestFullname,
-                status: Status.FAILED,
-                start: 1745926893309,
-                stop: 1745926894309,
-                duration: 1000,
-                labels: [],
-              },
-            },
-            metrics: {},
-          },
-        ],
-        historyPath: "history.jsonl",
-        knownIssuesPath: undefined,
-      },
-      testResults: [
-        // Classic flaky test
-        {
+    const historyItemsCount = 6;
+    // History: flaky: PASSED -> Rest is in FAILED state
+    const flakyHistoryResults = makeTestResults(historyItemsCount, (index) => {
+      if (index === 0) {
+        return {
           name: flakyTestName,
           fullName: flakyTestFullname,
-          historyId,
-          status: Status.FAILED,
-          stage: Stage.FINISHED,
-          start: now + 4000,
-          stop: now + 5000,
-        },
-        // Non-flaky test
-        {
-          name: "Non-flaky test",
-          fullName: "sample.js#Non-flaky test",
           status: Status.PASSED,
           stage: Stage.FINISHED,
-          start: now + 10000,
-          stop: now + 11000,
-        },
-      ],
+        };
+      }
+
+      return {
+        name: flakyTestName,
+        fullName: flakyTestFullname,
+        status: Status.FAILED,
+        stage: Stage.FINISHED,
+      };
+    });
+
+    // History: non-flaky: just always PASSED
+    const nonFlakyHistoryResults = makeTestResults(historyItemsCount, () => ({
+      name: nonFlakyTestName,
+      fullName: nonFlakyTestFullname,
+      status: Status.PASSED,
+      stage: Stage.FINISHED,
+    }));
+
+    const history = makeHistory(historyItemsCount, (index) => ({
+      name: reportName,
+      knownTestCaseIds: [flakyTestCaseId, nonFlakyTestCaseId],
+      testResults: {
+        ...makeHistoryTestResults([flakyHistoryResults[index]]),
+        ...makeHistoryTestResults([nonFlakyHistoryResults[index]]),
+      },
+    }));
+
+    // Current results: flaky (FAILED), non-flaky (PASSED)
+    const testResults = [
+      makeTestResult({
+        name: flakyTestName,
+        fullName: flakyTestFullname,
+        historyId: flakyHistoryId,
+        status: Status.FAILED,
+        stage: Stage.FINISHED,
+      }),
+      makeTestResult({
+        name: nonFlakyTestName,
+        fullName: nonFlakyTestFullname,
+        historyId: nonFlakyHistoryId,
+        status: Status.PASSED,
+        stage: Stage.FINISHED,
+      }),
+    ];
+
+    bootstrap = await bootstrapReport({
+      reportConfig: makeReportConfig({
+        name: reportName,
+        history,
+      }),
+      testResults,
     });
   });
 
