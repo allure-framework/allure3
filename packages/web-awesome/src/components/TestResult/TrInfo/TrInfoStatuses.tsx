ba64a89b765a6786410d3dcb2ef2d26dd533d533
Dmitrii S. <Viperwow@users.noreply.github.com>
2025-06-20 15:12:31 +0400

    feat: add Text component for title capitalization in TrInfoStatuses (#221)

--- packages/web-awesome/src/components/TestResult/TrInfo/TrInfoStatuses.tsx
+++ packages/web-awesome/src/components/TestResult/TrInfo/TrInfoStatuses.tsx
@@ -1,4 +1,4 @@
-import { SvgIcon, allureIcons } from "@allurereport/web-components";
+import { SvgIcon, allureIcons, Text } from "@allurereport/web-components";
 import { type FunctionalComponent } from "preact";
 import { useI18n } from "@/stores";
 import { capitalize } from "@/utils/capitalize";
@@ -21,7 +21,9 @@ export const TrInfoStatuses: FunctionalComponent<{ statuses: [string, boolean][]
         return (
           <div key={key} className={styles["test-result-info-status"]}>
             <SvgIcon className={styles["metadata-icon"]} id={icons[status]} size={"s"} />
-            {capitalize(title)}
+            <Text type={"ui"} size={"s"}>
+              {capitalize(title)}
+            </Text>
           </div>
         );
       })}
