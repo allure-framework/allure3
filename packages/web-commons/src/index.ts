0c7e03b1b59d1dd222e7e881ab720f5814614ad0
viperwow <dmitrii.spirin@qameta.io>
2025-07-29 13:11:37 +0400

    feat(web-commons): enhance chart utilities with new data structures and functions for trend and pie charts

--- packages/web-commons/src/index.ts
+++ packages/web-commons/src/index.ts
@@ -4,6 +4,8 @@ export * from "./attachments.js";
 export * from "./i18n.js";
 export * from "./strings.js";
 export * from "./charts.js";
+export * from "./charts/colors.js";
+export * from "./charts/utils.js";
 export * from "./charts/getPieChart.js";
 export * from "./charts/getPieChartDashboard.js";
 export * from "./charts/getSeverityTrendData.js";
