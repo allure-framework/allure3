8a6c70a6d4422f58ef3e444e4ea427d6b7823807
viperwow <dmitrii.spirin@qameta.io>
2025-07-30 17:32:46 +0400

    refactor(web-commons): enhance type imports and streamline chart data interfaces for better clarity

---
+++ packages/web-commons/src/charts/getPieChartDashboard.ts
@@ -0,0 +1,25 @@
+import type { Statistic, PieChartOptions, PieChartData } from "@allurereport/core-api";
+import { statusesList } from "@allurereport/core-api";
+import { d3Arc, d3Pie, getPercentage } from "../charts.js";
+
+export const getPieChartDataDashboard = (stats: Statistic, chartOptions: PieChartOptions): PieChartData => {
+  const convertedStatuses = statusesList
+    .filter((status) => !!stats?.[status])
+    .map((status) => ({
+      status,
+      count: stats[status]!,
+    }));
+  const arcsData = d3Pie(convertedStatuses);
+  const slices = arcsData.map((arcData) => ({
+    d: d3Arc(arcData),
+    ...arcData.data,
+  }));
+  const percentage = getPercentage(stats.passed ?? 0, stats.total);
+
+  return {
+    type: chartOptions.type,
+    title: chartOptions?.title,
+    slices,
+    percentage,
+  };
+};
