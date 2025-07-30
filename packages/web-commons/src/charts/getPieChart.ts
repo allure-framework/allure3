3d732acca0c57ff78b4d857957d6a220237830a8
viperwow <dmitrii.spirin@qameta.io>
2025-07-30 18:19:31 +0400

    refactor(web-awesome): update pie chart store and remove unused chart utilities for improved clarity and type safety

--- packages/web-commons/src/charts/getPieChart.ts
+++ packages/web-commons/src/charts/getPieChart.ts
@@ -5,11 +5,16 @@ import { d3Arc, d3Pie, getPercentage } from "../charts.js";
 export type TestResultSlice = {
   status: TestStatus;
   count: number;
+  d: string;
 };
 
 export type TestResultChartData = {
   percentage: number;
   slices: TestResultSlice[];
+};
+
+export const isTestResultSlice = (slice: TestResultSlice | null): slice is TestResultSlice => {
+  return slice !== null;
 };
 
 export const getPieChartData = (stats: Statistic): TestResultChartData => {
@@ -20,10 +25,20 @@ export const getPieChartData = (stats: Statistic): TestResultChartData => {
       count: stats[status]!,
     }));
   const arcsData = d3Pie(convertedStatuses);
-  const slices = arcsData.map((arcData) => ({
-    d: d3Arc(arcData),
-    ...arcData.data,
-  }));
+  const slices = arcsData
+    .map((arcData) => {
+      const d = d3Arc(arcData);
+
+      if (!d) {
+        return null;
+      }
+
+      return {
+        d,
+        ...arcData.data,
+      };
+    })
+    .filter(isTestResultSlice);
   const percentage = getPercentage(stats.passed ?? 0, stats.total);
 
   return {
