033c5c2a5360ac2811d79573fbdb9d913a2bc581
viperwow <dmitrii.spirin@qameta.io>
2025-08-11 01:17:44 +0400

    feat(web-components): implement BarChart component with configuration and theming support

--- packages/web-components/src/components/Charts/BarChart/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Bar } from "@nivo/bar";
-import type { BarSvgProps, BarDatum } from "@nivo/bar";
-import { commonProps } from "./constants";
-
-export type BarChartProps<D extends BarDatum> = BarSvgProps<D>;
-
-export const BarChart = <D extends BarDatum>(props: BarChartProps<D>) => {
-    return <Bar {...commonProps} {...props} />;
-};
