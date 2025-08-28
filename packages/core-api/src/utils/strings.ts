3f775ba85c55b64d1d2959a1bfb2622368232e1a
viperwow <dmitrii.spirin@qameta.io>
2025-08-28 18:19:05 +0400

    feat(core-api): add capitalize function for string manipulation

--- packages/core-api/src/utils/strings.ts
+++ packages/core-api/src/utils/strings.ts
@@ -3,5 +3,5 @@ export const capitalize = <T extends string>(str: T): Capitalize<T> | undefined 
     return;
   }
 
-  return (str.charAt(0).toLocaleUpperCase() + str.slice(1)) as Capitalize<T>;
+  return str.charAt(0).toLocaleUpperCase() + str.slice(1) as Capitalize<T>;
 };
