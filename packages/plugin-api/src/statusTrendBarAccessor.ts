b396513e53ee5e59a9b6be8dc080c4cedbf31aee
viperwow <dmitrii.spirin@qameta.io>
2025-08-28 18:27:59 +0400

    refactor(core-api): simplify history data retrieval by introducing htrsByTr function

--- packages/plugin-api/src/statusTrendBarAccessor.ts
+++ packages/plugin-api/src/statusTrendBarAccessor.ts
@@ -1,93 +1,81 @@
 import type { HistoryDataPoint, TestStatus, BarGroup, TestResult, HistoryTestResult } from "@allurereport/core-api";
-import { BarGroupMode } from "@allurereport/core-api";
+import { BarGroupMode, htrsByTr } from "@allurereport/core-api";
 import { type BarDataAccessor, createEmptyStats } from "./charts.js";
 
-type TrendKey = "passed" | "failed" | "broken";
+type TrendKey = Extract<TestStatus, "passed" | "failed" | "broken">;
 type TrendStats = Record<TrendKey, number>;
-type TestStatuses = Record<string, { status: TestStatus }>;
 
 const groupKeys = ["passed", "failed", "broken"] as const;
-const emptyStats = createEmptyStats(groupKeys);
-const NON_SIGNIFICANT_HISTORY_STATUSES = ["unknown", "skipped"];
 
 const isGroupKey = (key: string): key is TrendKey => groupKeys.includes(key as TrendKey);
 
 const getSignedValueByStatus = (status: TestStatus): number => status === "passed" ? 1 : -1;
 
-/**
- * @description Checks if the history test result has a significant status.
- * @param htr - The history test result to check.
- * @returns `true` if the history test result has a significant status, `false` otherwise.
- */
-const hasSignificantStatus = (htr: HistoryTestResult) => !NON_SIGNIFICANT_HISTORY_STATUSES.includes(htr.status);
+const hasSignificantStatus = (htr: HistoryTestResult): boolean => isGroupKey(htr.status);
 
-/**
- * @description Gets the most recent significant status from test history.
- * @param history - The history of test results
- * @returns The most recent significant status or undefined if none found
- */
-export const getLastSignificantStatus = (history: HistoryTestResult[] = []): string | undefined => {
+const getLastSignificantStatus = (history: HistoryTestResult[] = []): TestStatus | undefined => {
   const significantHtr = history.find(hasSignificantStatus);
 
   return significantHtr?.status;
 };
 
-const getTestStatuses = (testResults: (TestResult | HistoryTestResult)[]): TestStatuses => testResults.reduce((acc, test) => {
-  acc[test.id] = { status: test.status };
+const isDifferentStatuses = (currentStatus: TestStatus, lastSignificantStatus?: TestStatus): boolean => {
+  return !!lastSignificantStatus && isGroupKey(currentStatus) && isGroupKey(lastSignificantStatus) && currentStatus !== lastSignificantStatus;
+};
 
-  return acc;
-}, {} as TestStatuses);
+const getPointStats = (currentTrs: (TestResult | HistoryTestResult)[], hdps: HistoryDataPoint[]): TrendStats => {
+  const stats = createEmptyStats(groupKeys);
 
-const compareTestResults = (
-  previousTests: Record<string, { status: TestStatus }>,
-  currentTests: Record<string, { status: TestStatus }>,
-): TrendStats => {
-  const stats = { ...emptyStats };
+  for (const tr of currentTrs) {
+    const htrs = htrsByTr(hdps, tr);
 
-  const currentIds = new Set(Object.keys(currentTests));
-  const previousIds = new Set(Object.keys(previousTests));
+    const currentStatus = tr.status as TrendKey;
+    const lastSignificantStatus = getLastSignificantStatus(htrs);
 
-  // Find status changes for existing items in both previous and current points
-  for (const testId of currentIds) {
-    if (previousIds.has(testId)) {
-      const currentTest = currentTests[testId];
-      const previousTest = previousTests[testId];
-
-      if (currentTest?.status && previousTest?.status && currentTest.status !== previousTest.status) {
-        if (isGroupKey(currentTest.status) && isGroupKey(previousTest.status)) {
-          stats[currentTest.status] = stats[currentTest.status] + getSignedValueByStatus(currentTest.status);
-        }
-      }
+    if (isDifferentStatuses(currentStatus, lastSignificantStatus)) {
+      stats[currentStatus] = stats[currentStatus] + getSignedValueByStatus(currentStatus);
     }
   }
 
   return stats;
 };
 
-const getTrendData = (testResults: TestResult[], historyPoints: HistoryDataPoint[]): BarGroup<string, TrendKey>[] => {
-  const currentTestStatuses = getTestStatuses(testResults);
-  const historyAndCurrentTestResults = [{ testResults: currentTestStatuses }, ...historyPoints];
+const getCurrentStats = (testResults: TestResult[], hdps: HistoryDataPoint[]): BarGroup<string, TrendKey> => {
+  return {
+    groupId: "current",
+    ...getPointStats(testResults, hdps),
+  };
+};
 
+const getHistoricalStats = (hdps: HistoryDataPoint[]): BarGroup<string, TrendKey>[] => {
   const trendData: BarGroup<string, TrendKey>[] = [];
-  for (let i = 0; i < historyAndCurrentTestResults.length; i++) {
-    const previousHistoryPoint = i + 1 < historyAndCurrentTestResults.length ? historyAndCurrentTestResults[i + 1] : { testResults: {} };
-    const currentHistoryPoint = historyAndCurrentTestResults[i];
-    const pointIndex = historyAndCurrentTestResults.length - i - 1;
+
+  for (let i = 0; i < hdps.length; i++) {
+    const currentHdp = hdps[i];
+    const currentHdpTrs = Object.values(currentHdp.testResults);
+    const restHdps = i + 1 < hdps.length ? hdps.slice(i + 1, hdps.length) : [];
 
     trendData.push({
-      groupId: i === 0 ? "current" : `Point ${pointIndex}`,
-      ...compareTestResults(previousHistoryPoint.testResults, currentHistoryPoint.testResults),
+      groupId: `Point ${hdps.length - i - 1}`,
+      ...getPointStats(currentHdpTrs, restHdps),
     });
   }
 
   return trendData;
 };
 
+const getTrendData = (currentTrs: TestResult[], hdps: HistoryDataPoint[]): BarGroup<string, TrendKey>[] => {
+  const historicalStats = getHistoricalStats(hdps);
+  const currentStats = getCurrentStats(currentTrs, hdps);
+
+  return [currentStats, ...historicalStats];
+};
+
 export const statusTrendBarAccessor: BarDataAccessor<string, TrendKey> = {
-  getItems: async (store, historyPoints, isFullHistory) => {
+  getItems: async (store, hdps, isFullHistory) => {
     const testResults = await store.allTestResults();
 
-    let trendData = getTrendData(testResults, historyPoints);
+    let trendData = getTrendData(testResults, hdps);
 
     /* This is necessary not to exclude the last point that have been compared with the empty stats if the history is fully provided.
     *
