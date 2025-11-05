198ab1ff54c6ab47d0297d95c0cb39e4dc01212d
epszaw <konstantin@epishev.me>
2025-11-05 15:21:28 +0100

    Expose Allure config type in allure package (#344)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,7 +1,6 @@
 import { Builtins, Cli } from "clipanion";
-import console from "node:console";
 import { readFileSync } from "node:fs";
-import { argv, cwd } from "node:process";
+import { argv } from "node:process";
 import {
   Allure2Command,
   AwesomeCommand,
@@ -68,7 +67,5 @@ cli.register(ResultsUnpackCommand);
 cli.register(Builtins.HelpCommand);
 cli.runExit(args);
 
-console.log(cwd());
-
-export { defineConfig } from "@allurereport/plugin-api";
+export { type Config as AllureConfig, defineConfig } from "@allurereport/plugin-api";
 export { defaultChartsConfig } from "@allurereport/charts-api";
