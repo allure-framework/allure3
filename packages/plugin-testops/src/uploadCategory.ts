0ca77a16f08e6b674d382e205878d6a2d07fa9b5
Todti <hello@iamtodt.com>
2026-03-30 12:55:22 +0100

    add id to testops plugin and tests

--- packages/plugin-testops/src/uploadCategory.ts
+++ packages/plugin-testops/src/uploadCategory.ts
@@ -1,7 +1,7 @@
 import type { CategoryDefinition } from "@allurereport/core-api";
 
 import type { TestResultWithCategories, UploadCategory } from "./model.js";
-import { buildUploadCategoryGrouping, toUploadCategoryFromContext } from "./utils.js";
+import { buildUploadCategoryGrouping, toUploadCategoryFromContext } from "./utils/categories.js";
 
 export const toUploadCategory = (
   tr: TestResultWithCategories,
@@ -13,12 +13,16 @@ export const toUploadCategory = (
     const groupingFromTestResult = c.grouping?.length ? c.grouping : undefined;
     const contextCategory =
       groupingFromTestResult === undefined
-        ? contextCategories.find((category) => category.id === externalId || category.name === c.name)
+        ? contextCategories.find(
+            (category) => category.id === externalId || category.name === c.name,
+          )
         : undefined;
     return {
       externalId,
       name: c.name,
-      grouping: groupingFromTestResult ?? (contextCategory && buildUploadCategoryGrouping(tr, contextCategory)),
+      grouping:
+        groupingFromTestResult ??
+        (contextCategory && buildUploadCategoryGrouping(tr, contextCategory)),
     };
   }
   return toUploadCategoryFromContext(tr, contextCategories);
