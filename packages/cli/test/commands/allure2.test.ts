b8fb3c7171466b3bd4962ffc001be81d06fc160f
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-05-07 07:12:03 +0700

    feat(cli): multi-values and globs for resultsDir of allure2

--- packages/cli/test/commands/allure2.test.ts
+++ packages/cli/test/commands/allure2.test.ts
@@ -1,10 +1,10 @@
 import * as console from "node:console";
-import { existsSync } from "node:fs";
 import { exit } from "node:process";
 
 import { AllureReport, readConfig } from "@allurereport/core";
 import Allure2Plugin from "@allurereport/plugin-allure2";
 import { run } from "clipanion";
+import { glob } from "glob";
 import { type Mock, beforeEach, describe, expect, it, vi } from "vitest";
 
 import { Allure2Command } from "../../src/commands/allure2.js";
@@ -28,16 +28,17 @@ vi.mock("node:process", async (importOriginal) => ({
   ...(await importOriginal()),
   exit: vi.fn(),
 }));
-vi.mock("node:fs", async (importOriginal) => ({
-  ...(await importOriginal()),
-  existsSync: vi.fn(),
-}));
 vi.mock("@allurereport/core", async () => {
   const { AllureReportMock } = await import("../utils.js");
 
   return {
     readConfig: vi.fn(),
     AllureReport: AllureReportMock,
+  };
+});
+vi.mock("glob", async () => {
+  return {
+    glob: vi.fn(),
   };
 });
 
@@ -47,34 +48,24 @@ beforeEach(() => {
 
 describe("allure2 command", () => {
   it("should exit with code 1 when resultsDir doesn't exist", async () => {
-    (existsSync as Mock).mockReturnValueOnce(false);
+    (glob as unknown as Mock).mockResolvedValueOnce([]);
 
-    const command = new Allure2Command();
-
-    command.cwd = ".";
-    command.resultsDir = fixtures.resultsDir;
-
-    await command.execute();
+    await run(Allure2Command, ["allure2", "--cwd", ".", fixtures.resultsDir]);
 
     expect(console.error).toHaveBeenCalledWith(
-      expect.stringContaining(`The given test results directory doesn't exist: ${fixtures.resultsDir}`),
+      expect.stringContaining(`No test results directories found matching pattern: ${fixtures.resultsDir}`),
     );
     expect(exit).toHaveBeenCalledWith(1);
     expect(AllureReport).not.toHaveBeenCalled();
   });
 
   it("should initialize allure report with default plugin options when config doesn't exist", async () => {
-    (existsSync as Mock).mockReturnValueOnce(true);
     (readConfig as Mock).mockResolvedValueOnce({
       plugins: [],
     });
+    (glob as unknown as Mock).mockResolvedValueOnce([`${fixtures.resultsDir}/`]);
 
-    const command = new Allure2Command();
-
-    command.cwd = ".";
-    command.resultsDir = fixtures.resultsDir;
-
-    await command.execute();
+    await run(Allure2Command, ["allure2", "--cwd", ".", fixtures.resultsDir]);
 
     expect(AllureReport).toHaveBeenCalledTimes(1);
     expect(AllureReport).toHaveBeenCalledWith({
@@ -88,7 +79,6 @@ describe("allure2 command", () => {
   });
 
   it("should initialize allure report with default plugin options even when config exists", async () => {
-    (existsSync as Mock).mockReturnValueOnce(true);
     (readConfig as Mock).mockResolvedValueOnce({
       plugins: [
         {
@@ -105,13 +95,9 @@ describe("allure2 command", () => {
         },
       ],
     });
+    (glob as unknown as Mock).mockResolvedValueOnce([`${fixtures.resultsDir}/`]);
 
-    const command = new Allure2Command();
-
-    command.cwd = ".";
-    command.resultsDir = fixtures.resultsDir;
-
-    await command.execute();
+    await run(Allure2Command, ["allure2", "--cwd", ".", fixtures.resultsDir]);
 
     expect(AllureReport).toHaveBeenCalledTimes(1);
     expect(AllureReport).toHaveBeenCalledWith(
@@ -127,8 +113,8 @@ describe("allure2 command", () => {
   });
 
   it("should prefer CLI arguments over config and defaults", async () => {
-    (existsSync as Mock).mockReturnValueOnce(true);
     (readConfig as Mock).mockResolvedValueOnce({});
+    (glob as unknown as Mock).mockResolvedValueOnce([`${fixtures.resultsDir}/`]);
 
     await run(Allure2Command, [
       "allure2",
@@ -153,8 +139,8 @@ describe("allure2 command", () => {
   });
 
   it("should not overwrite readConfig values if no CLI arguments provided", async () => {
-    (existsSync as Mock).mockReturnValueOnce(true);
     (readConfig as Mock).mockResolvedValueOnce({});
+    (glob as unknown as Mock).mockResolvedValueOnce([`${fixtures.resultsDir}/`]);
 
     await run(Allure2Command, ["allure2", "./allure-results"]);
 
