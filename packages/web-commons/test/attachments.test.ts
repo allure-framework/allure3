ecbe13eef9cd54d7946ba595403c531d17381325
Dmitrii <dmitrii.evreinov@qameta.io>
2026-05-29 14:12:40 +0400

    feat(web-awesome): add support for HTTP Exchange attachments (#656)
    Co-authored-by: Dmitry Baev <baev@users.noreply.github.com>

--- packages/web-commons/test/attachments.test.ts
+++ packages/web-commons/test/attachments.test.ts
@@ -14,4 +14,9 @@ describe("attachmentType", () => {
   it("maps text/html to html", () => {
     expect(attachmentType("text/html")).toBe("html");
   });
+
+  it("recognizes HTTP Exchange attachments", () => {
+    expect(attachmentType("application/vnd.allure.http+json")).toBe("http");
+    expect(attachmentType("application/vnd.allure.http+json; charset=utf-8")).toBe("http");
+  });
 });
