7977bc784f96d09137098168722ffe0214503da4
Todti <hello@iamtodt.com>
2026-06-02 21:49:13 +0100

    fix(web-awesome): remove color swatches from ColorSchemePicker

--- packages/web-awesome/src/components/ColorSchemePicker/ColorSchemeItem.tsx
+++ packages/web-awesome/src/components/ColorSchemePicker/ColorSchemeItem.tsx
@@ -1,14 +1,7 @@
 import type { ThemeFamilyOption } from "@/stores/colorScheme";
 
-import * as styles from "./styles.scss";
-
 interface ColorSchemeItemProps {
   option: ThemeFamilyOption;
 }
 
-export const ColorSchemeItem = ({ option }: ColorSchemeItemProps) => (
-  <span className={styles.item}>
-    <span className={styles.swatch} style={option.preview ? { background: option.preview } : undefined} />
-    {option.label}
-  </span>
-);
+export const ColorSchemeItem = ({ option }: ColorSchemeItemProps) => <span>{option.label}</span>;
