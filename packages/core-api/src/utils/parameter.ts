8ae064a9b4d0d8f892190ad33743441b274f4c29
Dmitrii Evreinov <dmitrii.evreinov@qameta.io>
2026-06-26 16:34:18 +0400

    fix comment

--- packages/core-api/src/utils/parameter.ts
+++ packages/core-api/src/utils/parameter.ts
@@ -3,16 +3,10 @@ import type { TestParameter } from "../metadata.js";
 const maskedParameterPlaceholder = "<masked>";
 
 export const redactParameters = (parameters: readonly TestParameter[] | undefined): TestParameter[] => {
-  return (parameters ?? []).flatMap((parameter) => {
-    if (parameter.hidden) {
-      return [];
-    }
-
-    return [
-      {
-        ...parameter,
-        value: parameter.masked ? maskedParameterPlaceholder : parameter.value,
-      },
-    ];
-  });
+  return (parameters ?? [])
+    .filter((parameter) => !parameter.hidden)
+    .map((parameter) => ({
+      ...parameter,
+      value: parameter.masked ? maskedParameterPlaceholder : parameter.value,
+    }));
 };
