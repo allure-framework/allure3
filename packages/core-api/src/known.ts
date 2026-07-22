a533d100d897db42c60c48c2207249af1ea2f9fc
epszaw <konstantin@epishev.me>
2026-07-22 16:23:55 +0200

    add new type for quarantine test failures

--- packages/core-api/src/known.ts
+++ packages/core-api/src/known.ts
@@ -1,9 +1,12 @@
 import type { TestLink } from "./metadata.js";
 import type { TestError } from "./model.js";
 
-export interface KnownTestFailure {
+export interface QuarantineTestFailure {
   historyId: string;
+  error?: TestError;
+}
+
+export interface KnownTestFailure extends QuarantineTestFailure {
   issues?: TestLink[];
   comment?: string;
-  error?: TestError;
 }
