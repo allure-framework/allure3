be096817f8af110de9717f5e604931b2c0f84b7b
Dmitrii Evreinov <dmitrii.evreinov@qameta.io>
2026-09-17 12:00:58 +0400

    feat(web-awesome): make report metadata summary filter tree

--- packages/web-awesome/src/components/ReportMetadata/MetadataTestType.tsx
+++ packages/web-awesome/src/components/ReportMetadata/MetadataTestType.tsx
@@ -8,11 +8,11 @@ import * as styles from "@/components/ReportMetadata/styles.scss";
 
 export const MetadataTestType: FunctionComponent<MetadataProps> = ({ status, count }) => {
   return (
-    <div data-testid="metadata-value" className={styles["metadata-test-type"]}>
-      <div className={clsx(styles["metadata-color-badge"], styles?.[`status-${status}`])} />
+    <span data-testid="metadata-value" className={styles["metadata-test-type"]}>
+      <span className={clsx(styles["metadata-color-badge"], styles?.[`status-${status}`])} />
       <Text type={"ui"} size={"m"} bold>
         {count}
       </Text>
-    </div>
+    </span>
   );
 };
